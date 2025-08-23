56ac924506545bc5063cf527af57eb142a24fd57
kishikishore2863 <kishikishore.2863@gmail.com>
2025-08-23 23:28:00 +0530

    Pattern

---
+++ src/Pattern/p2.c
@@ -0,0 +1,20 @@
+//
+// Created by Kishi Kishore N on 23/08/25.
+//
+// C program to print a triangle
+#include <stdio.h>
+
+
+int main()
+{
+    int n = 6;
+
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
